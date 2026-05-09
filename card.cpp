8dfb02a5155edc2110a465ae324af8d1a9cedbd6
Roselin <wcallistis@gmail.com>
2026-05-08 18:39:04 -0700

    groundwork for method 2

--- card.cpp
+++ card.cpp
@@ -1,4 +1,41 @@
 // card.cpp
-// Author: Wren
+// Author: Wren Hzayen
 // Implementation of the classes defined in card.h
 
+#include "card.h"
+
+int suitRank(const std::string& suit) {
+    if (suit == "c") return 0;
+    if (suit == "d") return 1;
+    if (suit == "s") return 2;
+    if (suit == "h") return 3;
+    return -1;
+}
+
+int valueRank(const std::string& value) {
+    if (value == "a")  return 0;
+    if (value == "j")  return 10;
+    if (value == "q")  return 11;
+    if (value == "k")  return 12;
+    int n = std::stoi(value);
+    return n - 1;
+}
+
+// comparison operator
+bool Card::operator<(const Card& other) const {
+    int sr = suitRank(suit);
+    int sr2 = suitRank(other.suit);
+    if (sr != sr2) return sr < sr2;
+    return valueRank(value) < valueRank(other.value);
+}
+
+// equality operator
+bool Card::operator==(const Card& other) const {
+    return suit == other.suit && value == other.value;
+}
+
+// wait if stdout is just ostream, can't we modify the << operator to print a specific way when we use an ostream?
+std::ostream& operator<<(std::ostream& os, Card& c) {
+    os << c.suit << " " << c.value;
+    return os;
+}
--- card.h
+++ card.h
@@ -1,8 +1,27 @@
 // card.h
-// Author: Wren
+// Author: Wren Hzayen
 // All class declarations related to defining a single card go here
 
 #ifndef CARD_H
 #define CARD_H
+
+#include <string>
+#include <iostream>
+
+int suitRank(const std::string& suit);
+
+int valueRank(const std::string& value);
+
+struct Card {
+    std::string suit;
+    std::string value;
+
+    Card(std::string& s, std::string& v) : suit(s), value(v) {}
+
+    bool operator<(const Card& other) const;
+    bool operator==(const Card& other) const;
+};
+
+std::ostream& operator<<(std::ostream& os, Card& c);
 
 #endif
